b49b28c6a27178d9e1d7bbc210398dc988361887
mzaouchi <mzaouchi@gmail.com>
2024-02-29 12:40:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// document.getElementById('loubia').style.color = 'green'
+
+// var a = document.getElementById('loubia')
+// console.log(a)
+
+// a.style.color = 'red'
+
+// var b = document.getElementsByClassName('kafteji')
+// console.log(b)
+
+// b[0].style.color = 'green'
+// b[1].style.color = 'orange'
+
+// var c = document.getElementsByTagName('h2')
+// console.log(c)
+
+
+var div = document.getElementById('principal')
+// console.log(div)
+// console.log(div.innerHTML)
+// console.log(div.innerText)
+
+var mahmoud = document.createElement('button')
+mahmoud.innerText = "Jordan"
+div.appendChild(mahmoud)
