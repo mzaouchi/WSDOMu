b4c38e4adc3090123da8896923cbc33c5e67af3e
mzaouchi <mzaouchi@gmail.com>
2024-03-04 12:27:14 +0100

    Second Commit

--- script.js
+++ script.js
@@ -15,11 +15,57 @@
 // console.log(c)
 
 
-var div = document.getElementById('principal')
+// var div = document.getElementById('principal')
 // console.log(div)
 // console.log(div.innerHTML)
 // console.log(div.innerText)
 
-var mahmoud = document.createElement('button')
-mahmoud.innerText = "Jordan"
-div.appendChild(mahmoud)
+// var mahmoud = document.createElement('button')
+// mahmoud.innerText = "Jordan"
+// div.appendChild(mahmoud)
+
+// var titre = document.getElementById("loubia")
+// titre.style.color = "red"
+
+// var para = document.getElementsByClassName("kafteji")
+// console.log(para)
+// para[1].style.color = "orange"
+
+// var titre = document.querySelector("#loubia")
+// titre.style.color = 'red'
+
+// var para = document.querySelector(".kafteji")
+// para.style.color = "green"
+
+// var btnP = document.querySelector('.btnP')
+
+// btnP.addEventListener('click',function(){
+//     btnP.previousElementSibling.innerText++    
+// })
+
+// var btnM = document.querySelector('.btnM')
+
+// btnM.addEventListener('click',function(){
+//     if (btnM.nextElementSibling.innerText>0) {
+//         btnM.nextElementSibling.innerText--
+//     }  
+// })
+
+var btnPs = document.querySelectorAll(".btnP")
+
+for(let i = 0; i< btnPs.length; i++){
+    btnPs[i].addEventListener('click',function(){
+        btnPs[i].previousElementSibling.innerText++
+    })
+}
+
+var btnMs = document.querySelectorAll('.btnM')
+
+for(let i = 0; i<btnMs.length; i++){
+    btnMs[i].addEventListener('click',function(){
+
+        if(btnMs[i].nextElementSibling.innerText>0){
+            btnMs[i].nextElementSibling.innerText--
+        }        
+    })
+}
